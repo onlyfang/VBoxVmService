544f9be7814645fe928bbeb4589679fd8ce91ba0
Only Fang <only.fang@gmail.com>
2011-11-29 14:47:00 +0800

    released version 3.0 - Sweet Potato

---
+++ vms/src/package.bat
@@ -0,0 +1,31 @@
+@echo off
+
+set VERSION=3.0-Sweet_Potato
+
+mkdir vms
+copy ..\*.exe vms
+copy ..\VBoxVmService.ini vms
+mkdir vms\doc
+copy ..\doc\*.* vms\doc
+mkdir vms\src
+copy *.bat vms\src
+copy *.h vms\src
+copy *.c vms\src
+copy *.cpp vms\src
+copy *.rc vms\src
+copy *.sln vms\src
+copy *.vcproj vms\src
+mkdir vms\src\gfx
+copy gfx\*.* vms\src\gfx
+
+zip -r VBoxVmService-%VERSION%.zip vms
+
+del /q vms\src\gfx\*.*
+rmdir vms\src\gfx
+del /q vms\src\*.*
+rmdir vms\src
+del /q vms\doc\*.*
+rmdir vms\doc
+del /q vms\*.*
+rmdir vms
+

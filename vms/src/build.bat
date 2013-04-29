9813d3e23126432a44dfe19b40eef8c442ef0702
Only Fang <only.fang@gmail.com>
2013-04-29 19:54:14 +0800

    added empty Win32 project for VmServiceTray

--- vms/src/build.bat
+++ vms/src/build.bat
@@ -6,16 +6,19 @@ if "%1"=="release" goto RELEASE
 
 :DEBUG
 msbuild VboxVmService.vcxproj /t:Rebuild /p:Configuration=Debug;Platform=x64
+msbuild VmServiceTray.vcxproj /t:Rebuild /p:Configuration=Debug;Platform=Win32
 goto END
 
 :RELEASE
 msbuild VBoxVmService.vcxproj /t:Rebuild /p:Configuration=Release;Platform=Win32
-copy Release\*.exe ..
-msbuild VmServiceControl.vcxproj /t:Rebuild /p:Configuration=Release;Platform=Win32
-copy Release\*.exe ..
-
+copy Release\VBoxVmService.exe ..
 msbuild VBoxVmService.vcxproj /t:Rebuild /p:Configuration=Release;Platform=x64
 copy x64\Release\VBoxVmService.exe ..\VBoxVmService64.exe
+msbuild VmServiceControl.vcxproj /t:Rebuild /p:Configuration=Release;Platform=Win32
+copy Release\VmServiceControl.exe ..
+msbuild VmServiceTray.vcxproj /t:Rebuild /p:Configuration=Release;Platform=Win32
+copy Release\VmServiceTray.exe ..
+
 goto END
 
 :ERROR

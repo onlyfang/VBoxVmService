01c1a6cd821579addaab8842d2b75e8781d0b34c
Only <only.fang@gmail.com>
2019-05-12 18:36:08 +0800

    Build with Visual Studio 2019

--- do_build.bat
+++ do_build.bat
@@ -1,16 +1,18 @@
 @echo off
 @echo setup build environment
-if defined VS120COMNTOOLS call "%VS120COMNTOOLS%vsvars32.bat"
+if not defined DevEnvDir (
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+)
 
 if not exist build mkdir build
 cd build
-cmake ..
+cmake .. -G "Visual Studio 16 2019" -A Win32
 msbuild VBoxVmService.sln /p:Configuration=Release;Platform=Win32 /m
 cd ..
 
 if not exist build_x64 mkdir build_x64
 cd build_x64
-cmake .. -G "Visual Studio 12 2013 Win64"
+cmake .. -G "Visual Studio 16 2019" -A x64
 msbuild VBoxVmService.sln /p:Configuration=Release;Platform=x64 /m
 cd ..
 

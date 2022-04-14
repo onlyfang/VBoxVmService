4bcdf371886ed64c41dbe795b9cce51a8bb5efc6
Only <only.fang@gmail.com>
2022-04-15 07:59:14 +0800

    Compile with Visual Studio 2022

--- do_build.bat
+++ do_build.bat
@@ -1,15 +1,15 @@
 @echo off
 @echo setup build environment
 if not defined DevEnvDir (
-  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
 )
 
 if not exist build mkdir build
-cmake -B build -G "Visual Studio 16 2019" -A Win32
+cmake -B build -G "Visual Studio 17 2022" -A Win32
 cmake --build build --config Release
 
 if not exist build mkdir build_x64
-cmake -B build_x64 -G "Visual Studio 16 2019" -A x64
+cmake -B build_x64 -G "Visual Studio 17 2022" -A x64
 cmake --build build_x64 --config Release
 
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" setup.iss
